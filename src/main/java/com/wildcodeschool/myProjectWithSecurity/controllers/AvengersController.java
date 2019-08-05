997c7f9ec621bc21d3ca21b2783f93c043ad2d34
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-05 16:37:05 +0200

    added avengers controllers

---
+++ src/main/java/com/wildcodeschool/myProjectWithSecurity/controllers/AvengersController.java
@@ -0,0 +1,24 @@
+package com.wildcodeschool.myProjectWithSecurity.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AvengersController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the SHIELD";
+    }
+
+    @GetMapping("/avengers/assemble")
+    public String assemble() {
+        return "Avengers... Assemble";
+    }
+
+    @GetMapping("/secret-bases")
+    public String base() {
+        return "Reims, Paris, Bordeaux etc...";
+    }
+
+}
